635989e0952b4b950376bfbae4efb9c3d8f7dd65
SharmaAbhinandan <sharmaabhinandan@seasiainfotech.com>
2019-10-04 16:05:25 +0530

    acc screen

---
+++ app/src/main/java/com/oway/ui/home/dashboardaccount/EditProfileActivity.java
@@ -0,0 +1,16 @@
+package com.oway.ui.home.dashboardaccount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.oway.R;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
